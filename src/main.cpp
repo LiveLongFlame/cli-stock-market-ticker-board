1758bce790d0514c9d0574ff50b7ae001300ec45
Valentino Osorio Schwarz <s4091514@student.rmit.edu.au>
2025-12-01 22:08:20 +1100

    added todos

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,10 @@ int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
-    cout << "Hello world";
+    //todo: add ncurser lib 
+    //todo: add ikbr api and auth 
+
+
     
     return 0;
 }
